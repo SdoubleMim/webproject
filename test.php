bf3d60c57f2a8bf6763f48582ef7bfbb87880256
Matineh <matineh.mousavi8200@gmail.com>
2025-06-09 23:45:18 +0330

    the Final Structure of project!

--- test.php
+++
@@ -1,4 +0,0 @@
-<?php
-$db = new PDO("mysql:host=localhost;dbname=webproject_database", "root", "");
-$result = $db->query("SELECT COUNT(*) as count FROM users")->fetch();
-echo "Users in database: " . $result['count']; 
